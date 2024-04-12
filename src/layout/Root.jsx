ee000f5dbd1023256a0bb38e2955929b18598b1a
Md. Maynul Islam <islammaynul82@gmail.com>
2024-04-12 23:26:08 +0600

    added firebase setup and navbar

---
+++ src/layout/Root.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar";
+
+const Root = () => {
+    return (
+        <div className='font-display'>
+            <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default Root;
